6c402c218d59172a5d543e027e0c1ffa271afcf5
pancham kumar <panchamku8873@gmail.com>
2022-06-14 16:55:39 +0530

    Added employee service and controller

---
+++ src/controller/employeeController.js
@@ -0,0 +1,21 @@
+const EmployeeService = require("../services/employeeService");
+
+const update = async(req,res)=>{
+    try{
+        const user = await EmployeeService.update(req.body);
+        return res.status(200).json({
+            message : "successfullu updated employee",
+            success : true,
+            data: user
+        })
+    }catch(err){
+        return res.status(500).json({
+            message : "something went wrong",
+            success : false,
+            data: {}
+        })
+    }
+}
+module.exports = {
+ update
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const helpController = require("../../controller/helpController");
+const employeeController = require("../../controller/employeeController");
 
 router.get("/help",helpController.helpDetails);
+router.patch("/employee",employeeController.update);
 
 module.exports = router;
